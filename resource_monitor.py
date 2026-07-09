396ee4cdf64758687853ab13904d29dc2c1f8ad6
Wolos71 <105304486+Wolos71@users.noreply.github.com>
2026-07-09 14:49:16 +0200

    pętla i sprawdzanie systemu

--- resource_monitor.py
+++ resource_monitor.py
@@ -1,12 +1,30 @@
 import psutil
 from datetime import datetime
+import platform
+import time
 
 
-data_now = datetime.now()
-now = data_now.strftime("%Y-%m-%d %H:%M:%S")
+sys_name = platform.system()
 
-cpu = psutil.cpu_percent(interval=1)
-ram = psutil.virtual_memory().percent
 
-with open("/Users/wolos/Documents/project-monitor/log.csv", "a") as f:
-    f.write(f"{now}, {cpu}, {ram}")
+if sys_name != 'Linux':
+    path = "log.csv"
+else:
+    path = "/var/log/monitor/log.csv"
+
+
+while True:
+    data_now = datetime.now()
+    now = data_now.strftime("%Y-%m-%d %H:%M:%S")
+
+    cpu = psutil.cpu_percent(interval=1)
+    ram = psutil.virtual_memory().percent
+
+
+    try:
+        with open(path, "a") as f:
+            f.write(f"{now}, {cpu}, {ram}\n")
+    except PermissionError:
+        print("permission denied, run: sudo mkdir /var/log/monitor && chown $USER:$USER /var/log/monitor")
+
+        time.sleep(5)
